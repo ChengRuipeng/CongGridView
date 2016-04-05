131302a9e8237e6a3e759ad9b66206a70cacd860
congHu <850476264@qq.com>
2016-04-05 11:34:45 +0800

    class-base

---
+++ CongGirdView/CongGridView.swift
@@ -0,0 +1,115 @@
+//
+//  CongGridView.swift
+//  CongGirdView
+//
+//  Created by David on 16/4/5.
+//  Copyright (c) 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class CongGridView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addGrid()
+        
+    }
+    init(frame: CGRect, x: Int, y:Int) {
+        super.init(frame: frame)
+        numOfX = x
+        numOfY = y
+        addGrid()
+    }
+    private func addGrid(){
+        width = (frame.width-marginX)/CGFloat(numOfX)-marginX
+        height = (frame.height)/CGFloat(numOfY)-marginY
+        for i in 0..<numOfY{
+            cell.append([])
+            for j in 0..<numOfX{
+                var brick = UIView(frame: CGRectMake(marginX*CGFloat(j+1)+width*CGFloat(j), marginY*CGFloat(i)+height*CGFloat(i), width, height))
+                cell[i].append(brick)
+                cell[i][j].backgroundColor = defaultColor
+                self.addSubview(cell[i][j])
+            }
+        }
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    private var numOfX:Int = 6
+    var NumOfX:Int{
+        get{
+            return numOfX
+        }
+    }
+    private var numOfY:Int = 6
+    var NumOfY:Int{
+        get{
+            return numOfY
+        }
+    }
+    private var marginX:CGFloat = 2
+    var MarginX:CGFloat{
+        get{
+            return marginX
+        }
+        set(value){
+            marginX = value
+            reloadGrid()
+            
+        }
+    }
+    private var marginY:CGFloat = 2
+    var MarginY:CGFloat{
+        get{
+            return marginY
+        }
+        set(value){
+            marginY = value
+            reloadGrid()
+            
+        }
+    }
+    private var width:CGFloat!
+    private var height:CGFloat!
+    var cell:[[UIView!]] = []
+    private var defaultColor = UIColor.grayColor()
+    private func reloadGrid(){
+        for i in cell{
+            for j in i{
+                j.removeFromSuperview()
+            }
+        }
+        width = (frame.width-marginX)/CGFloat(numOfX)-marginX
+        height = (frame.height)/CGFloat(numOfY)-marginY
+        for i in 0..<numOfY{
+            for j in 0..<numOfX{
+                cell[i][j].frame = CGRectMake(marginX*CGFloat(j+1)+width*CGFloat(j), marginY*CGFloat(i)+height*CGFloat(i), width, height)
+                self.addSubview(cell[i][j])
+            }
+        }
+        
+    }
+    func resetFrame(frame:CGRect){
+        super.frame = frame
+        reloadGrid()
+    }
+    func setAllBGColor(c:UIColor){
+        for i in cell{
+            for j in i{
+                j.backgroundColor = c
+            }
+        }
+    }
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CongGirdView/ViewController.swift
+++ CongGirdView/ViewController.swift
@@ -10,51 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var numOfX:Int!
-    var numOfY:Int!
-    var marginX:CGFloat!
-    var marginY:CGFloat!
-    var blankBoard:CGFloat!
     
-    var containerWidth:CGFloat!
-    var containerHeight:CGFloat!
-    
-    var width:CGFloat!
-    var height:CGFloat!
-    
-    var wall:[[UIView!]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var cgv = CongGridView(frame: view.frame, x: 10, y: 9)
+        view.addSubview(cgv)
+        cgv.MarginX = 1
+        cgv.MarginY = 1
+        cgv.resetFrame(CGRectMake(20, 20, 300, 300))
+        cgv.setAllBGColor(UIColor.blackColor())
         
-        numOfX = 7
-        numOfY = 7
         
-        marginX = 2.0
-        marginY = 2.0
-        
-        blankBoard = 60.0
-        
-        if  NSString(string: UIDevice.currentDevice().systemVersion).doubleValue < 8.0{
-            containerHeight = view.bounds.width
-            containerWidth = view.bounds.height
-        }else{
-            containerWidth = view.bounds.width
-            containerHeight = view.bounds.height
-        }
-        
-        width = (containerWidth-marginX)/CGFloat(numOfX)-marginX
-        height = (containerHeight-200)/CGFloat(numOfY)-marginY
-        for i in 0..<numOfY{
-            wall.append([])
-            for j in 0..<numOfX{
-                var brick = UIView(frame: CGRectMake(marginX*CGFloat(j+1)+width*CGFloat(j), blankBoard+marginY*CGFloat(i)+height*CGFloat(i), width, height))
-                wall[i].append(brick)
-                wall[i][j].backgroundColor = UIColor.redColor()
-                view.addSubview(wall[i][j])
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
